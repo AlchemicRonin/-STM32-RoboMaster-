57615a3e69607527e06aa63d1d3663987e8d1bca
AlchemicRonin <1216532005@qq.com>
2020-03-10 20:37:42 -0500

    finished 教程01

---
+++ RoboMaster01/cmake-build-debug/CMakeFiles/RoboMaster01.elf.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Src/main.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/RoboMaster01.elf.dir/startup/startup_stm32f427xx.s.obj"
+  "RoboMaster01.elf"
+  "RoboMaster01.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/RoboMaster01.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
